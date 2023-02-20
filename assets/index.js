23ff51721c6bbce749d8bd60f7f75877fd2468ac
Pratik Kujur <kujurpratik115@gmail.com>
2023-02-20 23:48:08 +0530

    add comments

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,3 @@
-// const alarmTime=[];
 let alarms=[];
 const selectMenu = document.querySelectorAll("select");
 const currTime = document.querySelector("h1");
@@ -6,7 +5,6 @@ const setAlarmBtm =document.querySelector("button");
 const content= document.querySelector('.content');
 const alarmList = document.getElementById('list');
 const alarmCounter= document.getElementById('alarms-counter');
-const trash=document.querySelectorAll('.trash i');
 
 
 
@@ -14,6 +12,7 @@ let ringer=true;
 let isAlarmSet=false,
 ring =new Audio("./assets/Alarm.mp3");
 
+//options for the dropdown
 for(let i=12;i>0;i--){
     i= i<10 ? "0"+i : i;
     let option =`<option value="${i}">${i}</option>`;
@@ -30,6 +29,7 @@ for(let i=2;i>0;i--){
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend",option);
 }
 
+// clock checks time every second
 setInterval(()=>{
     let date =new Date(),
     h=date.getHours(),
@@ -48,6 +48,7 @@ setInterval(()=>{
     m= m<10 ? "0"+m : m;
     s= s<10 ? "0"+s : s;
 
+    //alarm time 
     currTime.innerText =`${h}:${m}:${s} ${ampm}`;
     for(let i=0;i<alarms.length;i++){
         if(alarms[i].text== `${h}:${m} ${ampm}` && ringer){
@@ -56,96 +57,92 @@ setInterval(()=>{
             content.classList.add("disabled");
             setAlarmBtm.innerText="Clear Alarm";
             isAlarmSet=true;
-            // console.log("ringing")
+            setAlarmBtm.style.backgroundColor='red';
         }
     }
 
 },1000);
 
-
-setAlarmBtm.addEventListener('click', ()=>{
-    if(isAlarmSet){
-        // alarmTime="";
-        let date =new Date();
-        ringer=false;
-        let s=(60 - date.getSeconds())*1000;
-        console.log(s);
-        setInterval(()=>{
-            ringer= true;
-        },s);
-        ring.pause();
-        content.classList.remove("disabled");
-        setAlarmBtm.innerText="Set Alarm";
-        return isAlarmSet=false;
-    }
-
-
+// function to set alarm
+function setAlarm(){
     let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
-    console.log(`${time}`);
-
+        
     if(time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")){
-        return alert("Please,select a valid time to set Alarm!");
+        return alert("Please, select a valid time to set Alarm!");
     }
     const alarm={
         text:time,
-        id:Date.now().toString(),
-        done:false
-        
+        id:Date.now().toString()   
     }
-    // alarmTime.push(alarm);
     addAlarm(alarm);
-   
-});
+
+}
+// adds alarm to the alarms list
 function addAlarm(alarm){
-    console.log(alarm);
     alarms.push(alarm);
     renderList();
 }
 
+// function to add the list of alarms to DOM
 function addAlarmToDOM(alarm){
     const li=document.createElement('li');
     li.innerHTML=`
-            <div>
-                <h4>Alarm</h4>
-                <p>${alarm.text}</p>
-            </div>
-            <div class="trash">
-            <i class="fa-solid fa-trash" data-id="${alarm.id}"></i></div>
+        <div>
+            <h4>Alarm</h4>
+            <p>${alarm.text}</p>
+        </div>
+        <div class="trash">
+        <i class="fa-solid fa-trash" data-id="${alarm.id}"></i></div>
     `;
- alarmList.append(li);
+    alarmList.append(li);
 }
 
+// function to render the list of alarms
 function renderList() {
     alarmList.innerHTML='';
 
-
     for(let i=0;i<alarms.length;i++){
-        console.log(alarms[i],'renderlist');
         addAlarmToDOM(alarms[i]);
     }
     alarmCounter.innerHTML=alarms.length;
 }
 
-// for(let i=0;i<trash.length;i++){
-//     trash[i].style.color='red';
-    document.addEventListener('click',(e)=>{
-        const target= e.target;
-        console.log(target);
-        if(target.className == 'fa-solid fa-trash' ){
-        const alarmId= target.dataset.id;
-        console.log(`alarmId:${alarmId}`);
-        deleteAlarm(alarmId);
-        }
-    });
-// }
+// function to clear alarm/ disable alarm
+function disableAlarm(){
+    let date =new Date();
+    ringer=false;
+    let s=(60 - date.getSeconds())*1000;
+    setInterval(()=>{
+        ringer= true;
+    },s);
+    ring.pause();
+    content.classList.remove("disabled");
+    setAlarmBtm.innerText="Set Alarm";
+    setAlarmBtm.style.backgroundColor='#4a98f7';
+    return isAlarmSet=false;
+}
 
+// function to delete the alarm from the list
 function deleteAlarm(alarmId){
     console.log(alarmId);
     const newAlarms = alarms.filter((alarm)=>{
-        // console.log(alarm.id!==alarmId);
-        
         return alarm.id!==alarmId;
     });
     alarms=newAlarms;
     renderList();
 }
+
+// event listener added to the document...
+document.addEventListener('click',(e)=>{
+    const target= e.target;
+    if(target.className == 'fa-solid fa-trash' ){
+        const alarmId= target.dataset.id;
+        deleteAlarm(alarmId);
+    }else if(target.className == 'button'){
+
+        if(isAlarmSet){
+            disableAlarm();   
+        }
+        setAlarm();
+    }
+});
